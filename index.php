d753fb7da3b10f1106154ce15682a6560c056c78
Kerim Žuna <85747649+KerimZuna@users.noreply.github.com>
2023-09-09 10:37:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Bootstrap Template</title>
+    <link href="styles.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona1.png" alt="Icon 1">
+                    <h6>UNOS</h6>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona2.png" alt="Icon 2">
+                    <h6>PREMJEŠTANJE</h6>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona3.png" alt="Icon 3">
+                    <h6>IZVJEŠTAJ</h6>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona4.png" alt="Icon 4">
+                    <h6>PRETRAGA</h6>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona5.png" alt="Icon 5">
+                    <h6>POSTAVKE</h6>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona6.png" alt="Icon 6">
+                    <h6>HISTORIJA</h6>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona7.png" alt="Icon 7">
+                    <h6>RAČUN</h6>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona8.png" alt="Icon 8">
+                    <h6>ODJAVA</h6>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona9.png" alt="Icon 9">
+                    <h6>SMJENA</h6>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="square">
+                    <img src="ikone/ikona10.png" alt="Icon 10">
+                    <h6>RADNIK</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
