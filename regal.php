ac90230d2d659296502b533d49b21a1a474d0400
FarisB1 <124277218+FarisB1@users.noreply.github.com>
2023-09-12 16:04:41 +0200

    Add files via upload

--- regal.php
+++ regal.php
@@ -3,6 +3,7 @@
 	<head>
 		<link rel="stylesheet" type="text/css" href="kutija.css?v=3"/>
 		<meta charset="UTF-8" name="viewport" content="width=device-width"/>
+        <title>WMS - AutoTarget - Dodaj regal</title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
