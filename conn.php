c72d867b23307cc8ef5e645cfa9cd19913232c40
KerimZuna <kerimzuna18@gmail.com>
2023-09-22 10:59:24 +0200

    23

--- conn.php
+++ conn.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 $servername = 'localhost';
 $username = 'root';
@@ -10,19 +9,3 @@ $conn = mysqli_connect($servername, $username, $password, "$db_name");
 if (!$conn) {
   die('Could not Connect MySql Server:' . mysql_error());
 }
-=======
-<?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = "dio";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, "$db_name");
-
-// Check connection
-if(!$conn){
-    die('Could not Connect MySql Server:' .mysql_error());
-  }
->>>>>>> 0ce7764a707c14f33bc28774473029c971784320
-?>
